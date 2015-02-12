6ac4492345d34d02973f1d4d3bcf63a2dd016568
msalguero <msalguero@unitec.edu>
2015-02-11 22:13:34 -0600

    Account and Register

---
+++ StackOverflow.Domain/Entities/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow.Domain.Entities
+{
+    public interface IEntity
+    {
+        Guid Id { get; }
+
+    }
+}
