7f4dc578a9f1d8ec61b72a93411221d208ca2f03
msalguero <msalguero@unitec.edu>
2015-03-14 14:56:54 -0600

    Rep

---
+++ StackOverflow.Web/Controllers/LoggingAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Mvc;
+using FilterAttribute = System.Web.Http.Filters.FilterAttribute;
+using IActionFilter = System.Web.Mvc.IActionFilter;
+
+namespace StackOverflow.Web.Controllers
+{
+    public class LoggingAttribute : FilterAttribute, IActionFilter, IResultFilter, IExceptionFilter
+    {
+
+        public void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnException(ExceptionContext filterContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
